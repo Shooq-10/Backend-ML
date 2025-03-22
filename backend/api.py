2f37bf5795d46c09d8a6a3d4c182fdf4405eff4c
Shooq-10 <146369096+Shooq-10@users.noreply.github.com>
2025-03-22 07:41:27 +0300

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -169,4 +169,4 @@ def predict():
     return jsonify({'predicted_price': predicted_price})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=6000)
+    app.run(debug=True, host="0.0.0.0", port=10000)
